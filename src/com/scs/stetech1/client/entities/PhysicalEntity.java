f4c98a7f88059d82a550183d784f8ac0ad3a26de
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2017-10-29 17:23:08 +0000

    Tweaks

--- src/com/scs/stetech1/client/entities/PhysicalEntity.java
+++ src/com/scs/stetech1/client/entities/PhysicalEntity.java
@@ -46,74 +46,8 @@ public abstract class PhysicalEntity extends Entity implements IProcessable {
 	}
 
 	public void clearPositiondata() {
-		/*synchronized (positionData) {
-			positionData.clear();
-		}*/
 		this.posCalc.clearPositiondata();
 	}
-
-	/*
-	public void addPositionData(EntityPositionData newData) {
-		synchronized (positionData) {
-			for(int i=0 ; i<this.positionData.size() ; i++) {
-				// Time gets earlier
-				EntityPositionData epd = this.positionData.get(i);
-				if (epd.serverTimestamp < newData.serverTimestamp) {
-					positionData.add(i, newData);
-					// Remove later entries
-					while (this.positionData.size() > i+3) {
-						this.positionData.remove(i+1);
-					}
-					return;
-				}
-			}
-			// Add to end
-			positionData.add(newData);
-		}
-	}
-
-
-	public void calcPosition(SorcerersClient mainApp, long serverTimeToUse) {
-		synchronized (positionData) {
-			if (this.positionData.size() > 1) {
-				EntityPositionData firstEPD = null;
-				for(EntityPositionData secondEPD : this.positionData) {
-					// Time gets earlier
-					if (firstEPD == null) {
-						firstEPD = secondEPD;
-						if (secondEPD.serverTimestamp < serverTimeToUse) {
-							return; // Too early!
-						}
-					} else if (firstEPD.serverTimestamp > serverTimeToUse && secondEPD.serverTimestamp < serverTimeToUse) {
-						// interpol between positions
-						float frac = (firstEPD.serverTimestamp - serverTimeToUse) / (serverTimeToUse - secondEPD.serverTimestamp);
-						Vector3f newPos = firstEPD.position.interpolate(secondEPD.position, frac);
-						if (module.getPlayersAvatar() == this) {
-							// if our avatar, adjust us, don't just jump to new position
-							// todo - check where we should be based on where we were 100ms ago
-							//todo - re-add newPos = newPos.interpolate(this.getWorldTranslation(), .5f); // Move us halfway?
-						}
-						//todo - re-add if (!newPos.equals(this.getWorldTranslation())) {
-						this.scheduleNewPosition(mainApp, newPos);
-						//}
-						if (module.getPlayersAvatar() == this) {
-							// if its our avatar, don't adjust rotation!
-						} else {
-							Quaternion newRot = new Quaternion();
-							final Quaternion newRot2 = newRot.slerp(firstEPD.rotation, secondEPD.rotation, frac);
-							// Set rotation in Callable
-							this.scheduleNewRotation(mainApp, newRot2);
-							Settings.p("Updated avatar pos: " + newPos);
-						}
-						return;
-					}
-				}
-				// If we got this far, all position data is too old!
-			}
-			//Settings.p("No position data for " + this.name + " (" + positionData.size() + " entries)");
-		}
-
-	}*/
 
 
 	public void scheduleNewPosition(SorcerersClient mainApp, final Vector3f newPos) {
--- src/com/scs/stetech1/shared/IEntityController.java
+++ src/com/scs/stetech1/shared/IEntityController.java
@@ -19,6 +19,6 @@ public interface IEntityController {
 	
 	void removeEntity(int id);
 	
-	IEntity getPlayersAvatar();
+	//IEntity getPlayersAvatar();
 	
 }
