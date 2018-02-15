e5aa094c2bba0b1018c8889b7edb6b9f32139af0
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2018-02-15 21:08:15 +0000

    Tweaked compile scripts

--- UndercoverAgent/compile_ua_server.sh
+++ UndercoverAgent/compile_ua_server.sh
@@ -1,4 +1,4 @@
 find -name "*.java" > sources.txt
-javac -d bin -cp ../stetech1/libs/jme/lib/gluegen-rt.jar @sources.txt $*
+javac -d bin -cp ../stetech1/libs/jme/gluegen-rt.jar:../stetech1/libs/jme/jME3-core.jar @sources.txt $*
 echo Finished.
 
