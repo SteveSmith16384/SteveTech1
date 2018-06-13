5eba608d0b060f6790a047eb7b4f7c45c25cd19d
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2018-06-13 12:39:50 +0100

    Moved TwoWeeks to its own project

--- TwoWeeks/src/twoweeks/server/ITerrainHeightAdjuster.java
+++
@@ -1,9 +0,0 @@
-package twoweeks.server;
-
-import com.jme3.terrain.heightmap.AbstractHeightMap;
-
-public interface ITerrainHeightAdjuster {
-
-	void adjustHeight(AbstractHeightMap heightmap);
-	
-}
